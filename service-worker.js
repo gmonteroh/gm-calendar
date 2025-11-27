6a85f68872cace0f7b8f566f6a190445e865ac91
gmonteroh <gmonteroh@gmail.com>
2025-11-26 22:44:33 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "gm-calendar-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
